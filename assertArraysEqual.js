c74228f0605383f49dc0b45f7ffb22517e569a08
Chibu <atuchibueze@gmail.com>
2022-04-05 20:55:31 -0400

    comparing arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    // console.log("length not same");
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      // console.log("diff elements");
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  const value = eqArrays(firstArray, secondArray);
+  if (value === true) {
+    console.log("good");
+  } else {
+    console.log("not good");
+  }
+}
+
+assertArraysEqual([1, 2, 3], [1, 3, 3]);
