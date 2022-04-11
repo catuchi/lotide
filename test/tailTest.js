788821faeb3d3d957e67a20c10d0bfcee4e92e03
Chibu <atuchibueze@gmail.com>
2022-04-11 17:53:12 -0400

    incorporated mocha and chai assertion libraries to generate tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,5 @@
 const assertEqual = require('../assertEqual');
+const tail = require('../tail');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
