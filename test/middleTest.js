5a235a87444f59150d1f463a668faa4bbf879a5d
Chibu <atuchibueze@gmail.com>
2022-04-11 19:10:21 -0400

    added tests to middleTest.js file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,20 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const { assert } = require('chai');
 
+describe("#middle", () => {
+  it("should return [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
 
-console.log(middle([1]));
-console.log(middle([1, 2]));
+  it("shoule return [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
 
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
+  it("should return [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
 
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  it("should return [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+});
